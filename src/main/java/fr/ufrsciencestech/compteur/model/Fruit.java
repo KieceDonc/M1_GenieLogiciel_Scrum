f44fd926fea4a94430e3a7a94756d2cb26495831
Alexandre PINON <ap380370@preseed.iem>
2022-10-10 16:42:04 +0200

    Remplissage des classes

--- src/main/java/fr/ufrsciencestech/compteur/model/Fruit.java
+++ src/main/java/fr/ufrsciencestech/compteur/model/Fruit.java
@@ -5,10 +5,14 @@ package fr.ufrsciencestech.compteur.model;
  */
 public interface Fruit {
     public boolean isSeedless();  //predicat indiquant si le fruit a ou non des pepins
+    
     public double getPrix();      //prix unitaire du fruit (en euros)
+    
     public String getOrigine();   //pays d'origine du fruit
+    
     @Override
     public boolean equals(Object o);  //predicat pour tester si 2 fruits sont equivalents
+    
     @Override
     public String toString();    //affichage d'un fruit
 }
