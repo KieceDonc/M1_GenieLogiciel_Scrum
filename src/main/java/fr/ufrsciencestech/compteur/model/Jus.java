e215dd503379eed0a6381bada1150f6de7774505
Cybermade <hamidounazim@gmail.com>
2022-10-10 16:57:54 +0200

    Add Jus

--- src/main/java/fr/ufrsciencestech/compteur/model/Jus.java
+++ src/main/java/fr/ufrsciencestech/compteur/model/Jus.java
@@ -1,13 +1,45 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package fr.ufrsciencestech.compteur.model;
-
 /**
- *
- * @author vr144361
+ * 
+ * @author Cybermade
  */
+
 public class Jus implements Fruit {
-    
+    private Fruit f;
+
+    Jus(Fruit f)
+    {
+        this.f = f;
+
+    }
+    @Override
+    public boolean isSeedless() {
+        // TODO Auto-generated method stub
+        return true;
+    }
+
+    @Override
+    public double getPrix() {
+        // TODO Auto-generated method stub
+        return f.getPrix()*1.2;
+    }
+
+    @Override
+    public String getOrigine() {
+        // TODO Auto-generated method stub
+        return f.getOrigine();
+    }
+    @Override
+    public String toString(){
+        return "Jus de " + f.toString() + " et le prix du jus est à " + f.getPrix()*1.2 + " euros";
+    }
+
+    public static void main (String[] args){
+        //Ecrire ici vos tests
+        Jus j = new Jus(new Banane());
+	       System.out.println("premier test jus");
+         
+         System.out.println(j.toString());
+
+   }
 }
